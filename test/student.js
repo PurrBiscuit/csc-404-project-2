b1f72a52db3063a24d4e98d40088f3d606a0f206
Purrbiscuit <spurr@articulate.com>
2021-04-24 15:20:13 -0400

    add the other test cases for the student library file

--- test/student.js
+++ test/student.js
@@ -1,5 +1,5 @@
+const { compose, dissoc, map } = require('ramda')
 const { expect } = require('chai')
-const { validate } = require('revalidator')
 
 const {
   filterQualified,
@@ -145,7 +145,7 @@ describe('student', () => {
 
     it('should have correct error message object', () => {
       const error = {
-        'courseGrades.csc141': 'Single letter grades accepted. Include \"-\" or \"+\" if necessary'
+        'courseGrades.csc141': 'Single letter grades accepted. Include "-" or "+" if necessary'
       }
 
       const { errors } = validateInput(invalidCSC141CourseGrade)
@@ -202,7 +202,7 @@ describe('student', () => {
 
     it('should have correct error message object', () => {
       const error = {
-        'courseGrades.csc142': 'Single letter grades accepted. Include \"-\" or \"+\" if necessary'
+        'courseGrades.csc142': 'Single letter grades accepted. Include "-" or "+" if necessary'
       }
 
       const { errors } = validateInput(invalidCSC142CourseGrade)
@@ -259,7 +259,7 @@ describe('student', () => {
 
     it('should have correct error message object', () => {
       const error = {
-        'courseGrades.csc240': 'Single letter grades accepted. Include \"-\" or \"+\" if necessary'
+        'courseGrades.csc240': 'Single letter grades accepted. Include "-" or "+" if necessary'
       }
 
       const { errors } = validateInput(invalidCSC240CourseGrade)
@@ -316,7 +316,7 @@ describe('student', () => {
 
     it('should have correct error message object', () => {
       const error = {
-        'courseGrades.csc241': 'Single letter grades accepted. Include \"-\" or \"+\" if necessary'
+        'courseGrades.csc241': 'Single letter grades accepted. Include "-" or "+" if necessary'
       }
 
       const { errors } = validateInput(invalidCSC241CourseGrade)
@@ -350,6 +350,248 @@ describe('student', () => {
       const { errors } = validateInput(missingCSC241CourseGrade)
 
       expect(errors).to.deep.equal(error)
+    })
+  })
+
+  describe('student record with name that needs to be normalized', () => {
+    const student = {
+      firstName: 'SalLy',
+      lastName: 'smith-SMartyPantsington',
+      courseGrades: {
+        csc141: 'A',
+        csc142: 'A-',
+        csc240: 'B+',
+        csc241: 'A-'
+      }
+    }
+
+    const normalizedStudent = {
+      ...student,
+      courseGrades: {
+        ...student.courseGrades
+      },
+      firstName: normalizeName('SalLy'),
+      lastName: normalizeName('smith-SMartyPantsington'),
+    }
+
+    console.log(normalizedStudent)
+
+    it('should be valid after normalization', () => {
+      const { valid } = validateInput(normalizedStudent)
+
+      expect(valid).to.be.true
+    })
+
+    it('should normalize the name properties on the record', () => {
+      const correctRecord = {
+        firstName: 'Sally',
+        lastName: 'Smith-Smartypantsington',
+        courseGrades: {
+          csc141: 'A',
+          csc142: 'A-',
+          csc240: 'B+',
+          csc241: 'A-'
+        }
+      }
+
+      expect(normalizedStudent).to.deep.equal(correctRecord)
+    })
+  })
+
+  describe('partialGPA is qualified or not', () => {
+    it('should qualify with a partial GPA of 2.5', () =>
+      expect(isGPAQualified(2.5)).to.be.true
+    )
+
+    it('should not qualify with a partial GPA of 1.0', () =>
+      expect(isGPAQualified(1.0)).to.be.false
+    )
+  })
+
+  describe('student records with partial GPA calculated', () => {
+    const students = [
+      {
+        firstName: 'Gary',
+        lastName: 'Blankenship',
+        partialGPA: 2.8
+      },
+      {
+        firstName: 'Freddie',
+        lastName: 'Mercury',
+        partialGPA: 3.8
+      },
+      {
+        firstName: 'Billy',
+        lastName: 'Borderline',
+        partialGPA: 2.5
+      },
+      {
+        firstName: 'Laura',
+        lastName: 'Peloton',
+        partialGPA: 2.1
+      },
+      {
+        firstName: 'Holden',
+        lastName: 'Caulfield',
+        partialGPA: 1.8
+      },
+      {
+        firstName: 'Rita',
+        lastName: 'Repulsa',
+        partialGPA: 3.5
+      },
+      {
+        firstName: 'Tommy',
+        lastName: 'Gunn',
+        partialGPA: 1.6
+      },
+      {
+        firstName: 'Bob',
+        lastName: 'Spongey',
+        partialGPA: 3.3
+      },
+      {
+        firstName: 'Jacqueline',
+        lastName: 'Maplesmith',
+        partialGPA: 4.0
+      }
+    ]
+
+    it('should filter out qualified students from unqualified ones', () => {
+      const correctRecords = [
+        {
+          firstName: 'Gary',
+          lastName: 'Blankenship'
+        },
+        {
+          firstName: 'Freddie',
+          lastName: 'Mercury'
+        },
+        {
+          firstName: 'Billy',
+          lastName: 'Borderline'
+        },
+        {
+          firstName: 'Rita',
+          lastName: 'Repulsa'
+        },
+        {
+          firstName: 'Bob',
+          lastName: 'Spongey'
+        },
+        {
+          firstName: 'Jacqueline',
+          lastName: 'Maplesmith'
+        }
+      ]
+
+      const removePartialGPAProp = map(dissoc('partialGPA'))
+
+      const qualifiedStudents = compose(removePartialGPAProp, filterQualified)(students)
+
+      expect(qualifiedStudents).to.deep.equal(correctRecords)
+    })
+  })
+
+  describe('unsorted list of student records', () => {
+    const unsortedStudents = [
+      {
+        firstName: 'Gina',
+        lastName: 'Notgonnacutit',
+        courseGrades: {
+          csc141: 'C',
+          csc142: 'C-',
+          csc240: 'F',
+          csc241: 'D+'
+        }
+      },
+      {
+        firstName: 'Billy',
+        lastName: 'Borderline',
+        courseGrades: {
+          csc141: 'C-',
+          csc142: 'C',
+          csc240: 'A-',
+          csc241: 'B'
+        }
+      },
+      {
+        firstName: 'Mary',
+        lastName: 'Meandering',
+        courseGrades: {
+          csc141: 'C',
+          csc142: 'A',
+          csc240: 'b-',
+          csc241: 'd+'
+        }
+      },
+      {
+        firstName: 'Hank',
+        lastName: 'Yule-Flunkington',
+        courseGrades: {
+          csc141: 'C',
+          csc142: 'C-',
+          csc240: 'D+',
+          csc241: 'F'
+        }
+      }
+    ]
+
+    it('should contain only valid student records', () => {
+      const invalidRecords = unsortedStudents.filter(student => {
+        const { valid } = validateInput(student)
+
+        return !valid
+      })
+
+      expect(invalidRecords).to.be.of.length(0)
+    })
+
+    it('should sort records in ascending order by the lastName property', () => {
+      const correctRecords = [
+        {
+          firstName: 'Billy',
+          lastName: 'Borderline',
+          courseGrades: {
+            csc141: 'C-',
+            csc142: 'C',
+            csc240: 'A-',
+            csc241: 'B'
+          }
+        },
+        {
+          firstName: 'Mary',
+          lastName: 'Meandering',
+          courseGrades: {
+            csc141: 'C',
+            csc142: 'A',
+            csc240: 'b-',
+            csc241: 'd+'
+          }
+        },
+        {
+          firstName: 'Gina',
+          lastName: 'Notgonnacutit',
+          courseGrades: {
+            csc141: 'C',
+            csc142: 'C-',
+            csc240: 'F',
+            csc241: 'D+'
+          }
+        },
+        {
+          firstName: 'Hank',
+          lastName: 'Yule-Flunkington',
+          courseGrades: {
+            csc141: 'C',
+            csc142: 'C-',
+            csc240: 'D+',
+            csc241: 'F'
+          }
+        }
+      ]
+
+      expect(sortStudents(unsortedStudents)).to.deep.equal(correctRecords)
     })
   })
 })
