8b6dee96da49aa26090f3a90a51a97583542e966
hzhong456 <68069530+hzhong456@users.noreply.github.com>
2021-04-25 11:47:19 -0400

    changed route to use mongo view

--- routes/qualified.js
+++ routes/qualified.js
@@ -6,36 +6,72 @@ const { students } = require('../lib/data')
 const { filterQualified, sortStudents } = require('../lib/student')
 const studentRecords = require('../lib/model/student_model')
 
+// /* GET qualified page. */
+// router.get('/', (req, res, next) => {
+
+//   studentRecords.find()
+//     .then(records => {
+//       const studentsWithGPAs = calcGPAs(records)
+//       const qualifiedStudents = filterQualified(studentsWithGPAs)
+//       const sortedStudents = sortStudents(qualifiedStudents)
+//       next()
+//       res.render('qualified', {
+//         students: sortedStudents,
+//         totalStudents: records.length
+//       })
+//       // next();
+//     })
+//     .catch(error => {
+//       console.log(`Error fetching users: ${error.message}`)
+//       next(error)
+
+//       // const studentsWithGPAs = calcGPAs(students)
+
+//       // const qualifiedStudents = filterQualified(studentsWithGPAs)
+
+//       // const sortedStudents = sortStudents(qualifiedStudents)
+
+//       // res.render('qualified', {
+//       //   students: sortedStudents,
+//       //   totalStudents: students.length
+//       // })
+//     })
+// })
+
+
 /* GET qualified page. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
+  res.render('qualified_mongo', {
+    students: 0,
+    totalStudents: 0
+  })
+})
+
+/* POST qualified page. */
+router.post('/', (req, res, next) => {
+
+  const gpaFilter = req.body.gpaFilter
+  console.log('GPA Filter:', gpaFilter)
 
   studentRecords.find()
     .then(records => {
       const studentsWithGPAs = calcGPAs(records)
-      const qualifiedStudents = filterQualified(studentsWithGPAs)
+      const qualifiedStudents = filterQualified(studentsWithGPAs, gpaFilter)
       const sortedStudents = sortStudents(qualifiedStudents)
-      next()
-      res.render('qualified', {
+
+      res.render('qualified_mongo', {
         students: sortedStudents,
-        totalStudents: records.length
+        totalStudents: records.length,
+        gpaFilter: gpaFilter
       })
-      // next();
+
     })
     .catch(error => {
       console.log(`Error fetching users: ${error.message}`)
       next(error)
-
-      // const studentsWithGPAs = calcGPAs(students)
-
-      // const qualifiedStudents = filterQualified(studentsWithGPAs)
-
-      // const sortedStudents = sortStudents(qualifiedStudents)
-
-      // res.render('qualified', {
-      //   students: sortedStudents,
-      //   totalStudents: students.length
-      // })
     })
+
 })
 
 module.exports = router
+
