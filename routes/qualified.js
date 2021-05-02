09cbbf37c0e42abded312b29f4130a0606cfcecd
Purrbiscuit <spurr@articulate.com>
2021-05-02 14:24:58 -0400

    camel case

--- routes/qualified.js
+++ routes/qualified.js
@@ -7,7 +7,7 @@ const { studentRecord } = require('../lib/model/studentModel')
 
 /* GET qualified page. */
 router.get('/', (req, res) => {
-  res.render('qualified_mongo', {
+  res.render('qualifiedMongo', {
     students: 0,
     totalStudents: 0
   })
@@ -15,9 +15,7 @@ router.get('/', (req, res) => {
 
 /* POST qualified page. */
 router.post('/', (req, res, next) => {
-
   const gpaFilter = req.body.gpaFilter
-  console.log('GPA Filter:', gpaFilter)
 
   studentRecord.find().lean()
     .then(records => {
